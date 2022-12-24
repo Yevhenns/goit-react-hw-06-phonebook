3ad299f98889da7c497ed0899c18eca82d747388
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-24 09:54:21 +0200

    123

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from './reducer';
 import { createAction, createReducer } from '@reduxjs/toolkit';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
