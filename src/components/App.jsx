5986e923cf3b9a1a992c1fa7dc79a8693402da7b
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-12-31 12:14:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Form } from './Form/Form';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
