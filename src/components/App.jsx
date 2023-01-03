5aa275c844219535e6374f27be8a9fb64e2e6d7a
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-03 10:02:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { Form } from './Form/Form';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
+import { getContactsArray } from 'redux/selectors';
+import { useSelector } from 'react-redux';
 
 import css from './App.module.css';
 
 export const App = () => {
+  const array = useSelector(getContactsArray);
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <Form />
       <h2>Contacts</h2>
-      <Filter />
-      <Contacts />
+      {array.length === 0 ? (
+        <p>No contacts to show</p>
+      ) : (
+        <>
+          <Filter />
+          <Contacts />
+        </>
+      )}
     </div>
   );
 };
