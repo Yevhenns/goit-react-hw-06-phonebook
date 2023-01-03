482c98cbd092c740fcca4da47a9059f1bb289ee1
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-03 11:24:06 +0200

    mistakes

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { ContactItem } from './ContactsItem/ContactsItem';
 import { useSelector } from 'react-redux';
-import { getContactsFilter, getContactsArray } from 'redux/selectors';
-import { nanoid } from '@reduxjs/toolkit';
+import { getFilter, getContacts } from 'redux/selectors';
 
 import css from './Contacts.module.css';
 
 export const Contacts = () => {
-  const contactsList = useSelector(getContactsArray);
-  const filter = useSelector(getContactsFilter);  
+  const contactsList = useSelector(getContacts);
+  const filter = useSelector(getFilter);  
 
   const filteredContacts = contactsList.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
     <ul className={css.list}>
       {filteredContacts.map(({ id, name, number }) => {
         return (
-          <li className={css.listItem} key={nanoid()}>
+          <li className={css.listItem} key={id}>
             <ContactItem
               id={id}
               name={name}
